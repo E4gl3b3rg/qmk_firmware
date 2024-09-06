e21d6085f6d36461bb08edee27e2474925840632
sashegyi <sashegyia@gmail.com>
2024-09-06 09:04:47 +0200

    m

--- keyboards/keychron/q8/ansi/keymaps/E4gl3b3rg/keymap.c
+++ keyboards/keychron/q8/ansi/keymaps/E4gl3b3rg/keymap.c
@@ -19,7 +19,7 @@
 // clang-format off
 
 enum layers {
-    WIN_BASE,
+    BASE,
     _FN1,
     _FN2,
     _FN3,
@@ -47,7 +47,7 @@ tap_dance_action_t tap_dance_actions[] = {
 #define KC_FLXP LGUI(KC_E)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [WIN_BASE] = LAYOUT_ansi_69(
+    [BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,       KC_EQL,   KC_BSPC,          KC_RCTL,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,       KC_RBRC,  KC_BSLS,          TD(TD_UP_HOME),
         KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,       KC_QUOT,  KC_ENT,           TD(TD_DOWN_END),
